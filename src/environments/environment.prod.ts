5879a4ac8187c9af4ea097a8834267064bbe790d
Luis Perez <laperezdelacruz@gmail.com>
2020-11-07 09:27:41 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBar4eK-M66cCVgvILtR_koXyE6UKN7Ub8",
+    authDomain: "ionic-ticket-lap.firebaseapp.com",
+    databaseURL: "https://ionic-ticket-lap.firebaseio.com",
+    projectId: "ionic-ticket-lap",
+    storageBucket: "ionic-ticket-lap.appspot.com",
+    messagingSenderId: "236359484089",
+    appId: "1:236359484089:web:af1c341c01580123b8d168",
+    measurementId: "G-Y9XYWXSTGC",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBar4eK-M66cCVgvILtR_koXyE6UKN7Ub8",
+    authDomain: "ionic-ticket-lap.firebaseapp.com",
+    databaseURL: "https://ionic-ticket-lap.firebaseio.com",
+    projectId: "ionic-ticket-lap",
+    storageBucket: "ionic-ticket-lap.appspot.com",
+    messagingSenderId: "236359484089",
+    appId: "1:236359484089:web:af1c341c01580123b8d168",
+    measurementId: "G-Y9XYWXSTGC",
+  },
 };
 
 /*
